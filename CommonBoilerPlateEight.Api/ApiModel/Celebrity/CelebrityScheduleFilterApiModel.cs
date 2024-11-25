41de6e8039361a76c6b1eb910e8e75a674ed27dc
common khadka <commonkhadka@gmail.com>
2024-11-25 22:28:02 +0545

    cleaned api proj

--- CommonBoilerPlateEight.Api/ApiModel/Celebrity/CelebrityScheduleFilterApiModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CommonBoilerPlateEight.Api.ApiModel
-{
-    public class CelebrityScheduleFilterApiModel
-    {
-        public int PageNumber { get; set; } = 1;
-        public DateTime? FromDate { get; set; }
-        public DateTime? ToDate { get; set; }
-    }
-}
